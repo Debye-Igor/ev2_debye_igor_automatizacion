b9532d7034e0821787e4d7e20e4ee62a8805bf7e
Debye-Igor <codes.harol@gmail.com>
2026-04-27 00:13:02 -0400

    see agrega clase Calculadora con metodos sumar y restar

---
+++ src/main/java/cl/iplacex/automatizacion/Calculadora.java
@@ -0,0 +1,17 @@
+package cl.iplacex.automatizacion;
+
+/**
+ * Operaciones aritméticas básicas.
+ * Clase usada como sujeto de prueba para demostrar
+ * pruebas unitarias atómicas e independientes.
+ */
+public class Calculadora {
+
+    public int sumar(int a, int b) {
+        return a + b;
+    }
+
+    public int restar(int a, int b) {
+        return a - b;
+    }
+}
